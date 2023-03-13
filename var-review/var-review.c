815242d5e5e2899cccd68b6ef74e995d2036e100
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-12 22:17:41 -0300

    var review

---
+++ var-review/var-review.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main(){
+int x;
+
+x = 5;
+
+printf("%d\n", x);
+x = 6;
+printf("%d", x);
+}
---
+++ var-review/var-review/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x;
+
+    x = 5;
+
+    printf("%d\n", x);
+
+    x =6;
+    printf("%d", x);
+}
