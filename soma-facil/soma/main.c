840a2c7d363da0cc1a4b031a77587b25cf5b60d5
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-14 16:23:35 -0300

    Soma Facil

---
+++ soma-facil/soma/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A, B;
+printf("Insira o primeiro valor: ");
+scanf("%d", &A);
+printf("Insira o segundo valor: ");
+scanf("%d", &B);
+printf("%d", A + B);
+return 0;
+}
