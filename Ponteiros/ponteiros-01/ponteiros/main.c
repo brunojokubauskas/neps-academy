43e65c0c15dc909a689bf16b8fd39e0a9cc2b532
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-17 02:09:35 -0300

    Ponteiros

---
+++ Ponteiros/ponteiros-01/ponteiros/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x;
+    int *p;
+    p = &x;
+    printf("%d\n", *p);
+    //Vai imprimir para onde o P esta apontando na posição de memoria,  nesse caso o X.
+
+
+
+}
