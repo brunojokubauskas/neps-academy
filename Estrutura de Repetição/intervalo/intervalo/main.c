4308c2348a021c7bf3fd9a12a16a86c1371d86ac
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-17 01:55:49 -0300

    Par ou Impar

--- Estrutura de Repetição/intervalo/intervalo/main.c
+++ Estrutura de Repetição/intervalo/intervalo/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    int x, y, maior, menor;
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+if(x > y){
+    maior = x;
+    menor = y;
+}else {
+    maior = y;
+    menor = x;
+}
+
+while(menor <= maior){
+    printf("%d", menor);
+    menor++;
+}
+
+return 0;
 }
---
+++ Estruturas de Decisão/parouimpar/parouimpar/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int B, C, soma;
+    scanf("%d", &B);
+    scanf("%d", &C);
+    soma = B + Ca;
+
+if(soma%2==0){
+    printf("Bino");
+}else{
+    printf("Cino");
+}
+return 0;
+}
