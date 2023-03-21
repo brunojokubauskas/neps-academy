a3549643191c0a27bf9426ad9b89a3f3aa86de16
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-21 15:50:12 -0300

    erro de digitação

--- Vetores/01/busca-simples/main.c
+++ Vetores/01/busca-simples/main.c
@@ -7,7 +7,7 @@ int main(){{
     for(int i = 0; i < 10;i++){
         scanf("%d", &vetor[i]);
     }
-    scanf("%d", &d);
+    scanf("%d", &x);
 
     for(int i = 0; i < 10;i ++){
         if(vetor[i] == x){
