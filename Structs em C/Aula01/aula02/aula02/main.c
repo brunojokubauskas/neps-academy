0513df27fe6417b6cf90b5c55746e309dd2326d9
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-04-17 16:37:58 -0300

    Add função teste com ponteiro

--- Structs em C/Aula01/aula02/aula02/main.c
+++ Structs em C/Aula01/aula02/aula02/main.c
@@ -6,12 +6,19 @@ struct par {
     int y;
 };
 typedef struct par par;
+//A função está recebendo uma função de memória...
+void teste(par *p){
+    p -> x = 2;
+    p -> y = 3;
+}
 int main(){
     par x;
     par *p = &x;
 
     //Acessando a partir de uma posição de  memória...
+
     p -> x = 1;
+    teste(&x);
     x.y = 2;
     printf("%d %d\n", x.x, x.y);
 }
